980f9db9310df029749123086c693e78e9a40926
LKlemens <klemens.lukaszczyk@gmail.com>
2024-03-12 20:11:39 +0100

    format fix

--- test/phoenix_hello_web/controllers/error_html_test.exs
+++ test/phoenix_hello_web/controllers/error_html_test.exs
@@ -9,6 +9,7 @@ defmodule PhoenixHelloWeb.ErrorHTMLTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(PhoenixHelloWeb.ErrorHTML, "500", "html", []) == "Internal Server Error"
+    assert render_to_string(PhoenixHelloWeb.ErrorHTML, "500", "html", []) ==
+             "Internal Server Error"
   end
 end
